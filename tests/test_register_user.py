c816591a80b590c6c1ff9172726ed6fb4618014e
Some Name <thatspock@gmail.com>
2023-06-02 23:01:06 +0900

    create register user raw test

--- tests/test_register_user.py
+++ tests/test_register_user.py
@@ -2,14 +2,16 @@ import time
 
 from selene import browser, have
 
+login = 'mrspock'
+
 
 def test_register_user():
     browser.open('https://automationexercise.com')
     browser.element('.shop-menu').should(have.text('Home'))
     browser.element('.fa-lock').click()
     browser.element('.signup-form').should(have.text('New User Signup!'))
-    browser.element('input[data-qa="signup-name"]').type('mrspock')
-    browser.element('input[data-qa="signup-email"]').type('hi@enterprise.com')
+    browser.element('input[data-qa="signup-name"]').type(login)
+    browser.element('input[data-qa="signup-email"]').type('hey9@enterprise.com')
     browser.element('button[data-qa="signup-button"]').click()
     browser.element('#form').should(have.text('ENTER ACCOUNT INFORMATION'))
     browser.element('#uniform-id_gender1').click()
@@ -19,4 +21,19 @@ def test_register_user():
     browser.element('#years').should(have.text('Year')).element('option[value="1985"]').click()
     browser.element('label[for="newsletter"]').click()
     browser.element('label[for="optin"]').click()
-    time.sleep(5)
+    browser.element('#first_name').type('mr')
+    browser.element('#last_name').type('spock')
+    browser.element('#company').type('enterprise')
+    browser.element('#address1').type('Market street 13')
+    browser.element('option[value="Australia"]').click()
+    browser.element('#state').type('NSW')
+    browser.element('#city').type('Sydney')
+    browser.element('#zipcode').type('2000')
+    browser.element('#mobile_number').type('0456904510')
+    browser.element('button[data-qa="create-account"]').click()
+    browser.element('#form').should(have.text('ACCOUNT CREATED!'))
+    browser.element('a[data-qa="continue-button"]').click()
+    browser.element('#header').should(have.text(login))
+    browser.element('.fa-trash-o').click()
+    browser.element('#form').should(have.text('ACCOUNT DELETED!'))
+    browser.element('a[data-qa="continue-button"]').click()
