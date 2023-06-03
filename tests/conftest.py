a772881d1fa30cfb1b5606f6c1c12fbf5b5b9987
Some Name <thatspock@gmail.com>
2023-06-03 23:07:55 +0900

    implement conftest and attachments

---
+++ tests/conftest.py
@@ -0,0 +1,53 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+from tests.constants import DEFAULT_BROWSER_VERSION
+from utils.attachment import AllureAttachmentManager
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser-version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_management(request):
+    browser_version = request.config.getoption('--browser-version')
+    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
+    options = Options()
+    selenoid_capabilities = {
+        'browserName': 'chrome',
+        'browserVersion': browser_version,
+        'selenoid:options': {
+            'enableVNC': True,
+            'enableVideo': True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
+    driver = webdriver.Remote(
+        command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
+        options=options)
+
+    browser = Browser(Config(driver=driver))
+
+    yield browser
+
+    allure_attachments = AllureAttachmentManager(browser)
+    allure_attachments.gather_all_attachments()
--- tests/constants.py
+++ tests/constants.py
@@ -1,1 +1,2 @@
 URL = 'https://automationexercise.com'
+DEFAULT_BROWSER_VERSION = "100.0"
