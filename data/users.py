0c5938321bd2152699845aec94c148741be58c3f
Some Name <thatspock@gmail.com>
2023-06-04 00:53:01 +0900

    Update users.py

--- data/users.py
+++ data/users.py
@@ -20,8 +20,8 @@ class User:
 
 
 admin = User(
-    login='mrspock',
-    email='hello@enterprise.com',
+    login=os.getenv('USER_LOGIN'),
+    email=os.getenv('USER_EMAIL'),
     password=os.getenv('USER_PASSWORD'),
     date_of_birth=['13', '5', '1985'],
     first_name='Mr',
